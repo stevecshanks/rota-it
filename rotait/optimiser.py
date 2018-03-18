159ecb46b04784916b7e2cf546ee4f541280cfb6
Steve Shanks <steve.c.shanks@gmail.com>
2018-03-18 21:16:05 +0000

    Improve clarity of already-assigned tasks

--- rotait/optimiser.py
+++ rotait/optimiser.py
@@ -9,19 +9,20 @@ class Optimiser:
         self.workload = Workload()
 
     def optimise(self):
-        for task in self._get_tasks_by_difficulty():
+        for task in self._sort_by_difficulty(self._get_unassigned()):
             eligible_people = self._get_eligible_people(task)
             best_person = self.workload.get_least_busy_person(eligible_people)
             if best_person:
                 self.workload.assign(task, best_person)
 
-    def _get_tasks_by_difficulty(self):
-        return sorted(self.tasks,
+    def _get_unassigned(self):
+        return [task for task in self.tasks if task.assignee is None]
+
+    def _sort_by_difficulty(self, tasks):
+        return sorted(tasks,
                       key=lambda task: len([p for p in self._get_eligible_people(task)]))
 
     def _get_eligible_people(self, task):
-        if task.assignee:
-            return [task.assignee]
         return [person for person in self.people
                 if person.has_skill(task.skill)
                 and person.is_available_on(task.date)]
